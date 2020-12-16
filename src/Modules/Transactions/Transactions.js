887df5fc1e055a3e9ff108e6976c854a1bcc62f4
prakash <prakash@divergentcs.org>
2020-12-16 06:13:49 +0530

    updated transactions

--- src/Modules/Transactions/Transactions.js
+++ src/Modules/Transactions/Transactions.js
@@ -1,14 +1,45 @@
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
+import { Component } from "react";
 import { Table } from "antd";
 
-const Transactions = () => {
-  return <>Transactions</>;
-};
+const { Column } = Table;
+
+class Transactions extends Component {
+  state = { selectedRowKeys: [] };
+  onSelectChange = (selectedRowKeys) => {
+    this.setState({ selectedRowKeys });
+  };
+  render() {
+    const columns = [
+      { title: "Id", dataIndex: "id" },
+      { title: "Amount", dataIndex: "Amount" },
+      { title: "Status", dataIndex: "Status" },
+      {
+        title: "Date",
+        dataIndex: "Date",
+        render: (record) => {
+          return new Date(record.seconds * 1000).toLocaleString("en-IN");
+        },
+      },
+    ];
+    const { selectedRowKeys } = this.state;
+    const rowSelection = { selectedRowKeys, onChange: this.onSelectChange };
+    return (
+      <>
+        <Table
+          rowSelection={rowSelection}
+          columns={columns}
+          dataSource={this.props.transactions}
+        />
+      </>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
-  return { transactions: state.transactions.transactions };
+  return { transactions: state.firestore.ordered.transactions };
 };
 
 export default compose(
